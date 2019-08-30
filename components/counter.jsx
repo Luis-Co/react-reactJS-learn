501b0d2b7509c4db68ce30f79400d81320a8b0d2
Luis-Co <corea3000@gmail.com>
2019-08-30 16:04:21 -0600

    1 file modified

--- components/counter.jsx
+++ components/counter.jsx
@@ -13,10 +13,12 @@ fontWeight: "bold"
 };
 
   render() {
+const classes = 'badge m-2 badge-';
+classes += (this.state.count === 0) ? "warning" : "primary";
+
     return( 
-    <div>
-   
-    <h1 style={this.styles} className="badge badge-warning m-2">{this.formatCount()}</h1>
+    <div>   
+    <h1 style={this.styles} className={classes}>{this.formatCount()}</h1>
     <button className="btn btn-info btn-sm">Increment</button>
     </div>
     );
